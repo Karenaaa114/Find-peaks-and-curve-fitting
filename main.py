bc62d2e888626cd1ca09a9910802713288bd91e2
Karena <lxc429233520@sina.com>
2022-07-25 18:58:42 +0100

    separate the x and y axis

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # filename = "NH4OH-FAU-Practice-data.csv"
+# data = read_data(filename)
 def read_data(filename):
-    """Return the data from csvfile and transpose the data to read by row.
-       Input: csv file
+    """Return the data from csv file and transpose the data to read by row.
+
+    Args:
+        filename (.csv file)
+
+    Returns:
+        data_transposed: data
     """
+
     data = []
     with open(filename, 'r') as csvfile:
         csv_reader = csv.reader(csvfile)  
@@ -16,3 +23,16 @@ def read_data(filename):
     data_transposed = np.array(data).transpose()
     data_transposed = data_transposed.astype(np.float32)
     return data_transposed 
+
+def separate_x_y_axis(data):
+    """To separate the data into two-theta(x-axis) and intensities(y-axis).
+
+    Args:
+        data (2-D array): the first line is two_theta(x-axis) and others are intensity(y-axis).
+
+    Returns:
+        two_theta, intensity: two theta(x-axis) and intensities in different temperature(y-axis).
+    """
+    two_theta = data[0]
+    intensity = data[1:]
+    return two_theta, intensity
