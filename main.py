31e075baa6897fdd18cbfd802fd5a985be7de18d
Karena <lxc429233520@sina.com>
2022-08-09 21:27:41 +0100

    add main.py file

---
+++ main.py
@@ -0,0 +1,24 @@
+from method import(
+    read_data,
+    separate_x_y_axis,
+    plot_data,
+    plot_data_log10,
+    baseline_als,
+    get_index_in_interval,
+    interval_data,
+    gaussian_fitting_curve,
+)
+
+
+def main():
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
