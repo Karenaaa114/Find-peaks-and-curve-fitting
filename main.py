0ecc0337d93e9a249430f5f3bd8717660bddac05
Karena <lxc429233520@sina.com>
2022-08-10 14:10:59 +0100

    add file name etc to def main()

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ from method import(
 
 
 def main():
+    filename = "ZnO transformation.csv"
+    data = read_data(filename)
+    two_theta, intensity = separate_x_y_axis(data)
+    plot_data(two_theta, intensity)
+    gaussian_fitting_plot(two_theta,intensity[50],[20,35],[[26.4,1,600],[28.4,1,750],[30,1,750]])
+    gaussian_plot_error(two_theta,intensity[60],[20,35],[[26.4,1,600],[28.4,1,750],[30,1,750]])
+
+
 
 
 
