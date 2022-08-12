b4edc536213978564efbe47198f2b73c790262e7
Karena <lxc429233520@sina.com>
2022-08-12 10:06:05 +0100

    change filename = "PdCeO2" into main()

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 from method import(
     read_data,
+    open_file,
     separate_x_y_axis,
     plot_data,
     plot_data_log10,
@@ -24,15 +29,30 @@ from method import(
 
 
 def main():
-    filename = "ZnO transformation.csv"
-    data = read_data(filename)
+    # filename = "ZnO transformation.csv"
+    # data = read_data(filename)
+    # filename = "PdCeO2"
+    # data = open_file(filename)
+    # two_theta, intensity = separate_x_y_axis(data)
+    # plot_data(two_theta, intensity)
+    # gaussian_fitting_plot(two_theta,intensity[50],[20,35],[[26.4,1,600],[28.4,1,750],[30,1,750]])
+    # gaussian_plot_error(two_theta,intensity[60],[20,35],[[26.4,1,600],[28.4,1,750],[30,1,750]])
+
+
+
+
+    filename = "PdCeO2"
+    data = open_file(filename)
     two_theta, intensity = separate_x_y_axis(data)
     plot_data(two_theta, intensity)
-    gaussian_fitting_plot(two_theta,intensity[50],[20,35],[[26.4,1,600],[28.4,1,750],[30,1,750]])
-    gaussian_plot_error(two_theta,intensity[60],[20,35],[[26.4,1,600],[28.4,1,750],[30,1,750]])
+    #plot every dataset in the folder
+    for i in range(len(intensity)):
+        plt.title('Gaussian fitting for dataset %d' %i)
+        gaussian_fitting_plot(two_theta,intensity[i],[1.8,4.2],[[2.4,0.038,0.3],[3.8,0.07,1.13]])
 
-
-
+    gaussian_plot_error(two_theta,intensity[3],[1.8,4.2],[[2.4,0.038,0.3],[3.8,0.07,1.13]])
+    lorentzian_plot_error(two_theta,intensity[3],[1.8,4.2],[[2.4,0.038,0.3],[3.8,0.07,1.13]])
+    PseudoVoigt_plot_error(two_theta,intensity[3],[1.8,4.2],[[2.4,0.038,0.3],[3.8,0.07,1.13]])
 
 
 
